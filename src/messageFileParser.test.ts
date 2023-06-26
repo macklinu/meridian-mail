5c8ba2f9bf4a254b8a18f5ac001e68c24722c14a
mackie <macklinu@users.noreply.github.com>
2023-06-26 13:09:27 -0400

    Set default time zone in test

--- src/messageFileParser.test.ts
+++ src/messageFileParser.test.ts
@@ -2,17 +2,22 @@ import * as crypto from 'node:crypto'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import * as process from 'node:process'
-import { expect, test } from 'vitest'
+import { beforeAll, expect, test } from 'vitest'
 import { messageFileParser } from './messageFileParser'
-
-// @ts-expect-error - crypto is not available in the browser; mocking for tests
-window.crypto = crypto
+import { Settings } from 'luxon'
 
 const dataPath = (fileName?: string) =>
   path.join(process.cwd(), 'src/data', fileName ?? '')
 
+beforeAll(() => {
+  // @ts-expect-error - crypto is not available in the browser; mocking for tests
+  window.crypto = crypto
+
+  Settings.defaultZone = 'utc'
+})
+
 test.each(fs.readdirSync(dataPath()))(
-  'msgFileParser("src/data/%s")',
+  'messageFileParser("src/data/%s")',
   async (fileName) => {
     const testFile = new File(
       [fs.readFileSync(dataPath(fileName), 'utf-8')],
